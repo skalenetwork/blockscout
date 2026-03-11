1e2e54e846f39e29f8e13a03eaa2ce687d007c46
Vadim Yavorsky <yavrsky@gmail.com>
2026-03-11 16:39:32 +0000

    fix: remove unused burn address hash string

--- apps/explorer/lib/explorer/chain.ex
+++ apps/explorer/lib/explorer/chain.ex
@@ -121,7 +121,6 @@ defmodule Explorer.Chain do
   @revert_msg_prefix_5 "execution reverted: "
   @revert_msg_prefix_6_empty "execution reverted"
 
-  @burn_address_hash_str "0x0000000000000000000000000000000000000000"
   @out_of_gas_msg "Out of gas"
 
   @limit_showing_transactions 10_000
