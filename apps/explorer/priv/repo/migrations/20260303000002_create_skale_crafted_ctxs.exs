3ecf1877aec1ee81210f7b18b1633fa81e71cfca
Vadim Yavorsky <yavrsky@gmail.com>
2026-03-30 17:03:19 +0100

    fix: rename derived_transaction_hash to crafted_transaction_hash for clarity

--- apps/explorer/priv/repo/migrations/20260303000002_create_skale_crafted_ctxs.exs
+++ apps/explorer/priv/repo/migrations/20260303000002_create_skale_crafted_ctxs.exs
@@ -4,7 +4,7 @@ defmodule Explorer.Repo.Migrations.CreateSkaleCraftedCtxs do
   def change do
     create table(:skale_crafted_ctxs, primary_key: false) do
       add(:origin_transaction_hash, :bytea, null: false, primary_key: true)
-      add(:derived_transaction_hash, :bytea, null: false, primary_key: true)
+      add(:crafted_transaction_hash, :bytea, null: false, primary_key: true)
 
       timestamps(null: false)
     end
